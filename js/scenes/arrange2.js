59b0ac3456da8b4754aa2f2421fc19480d43c934
Ken Perlin <ken.perlin@gmail.com>
2026-03-25 15:55:58 -0400

    Fixed multiplayer logic of arrange2.js.

--- js/scenes/arrange2.js
+++ js/scenes/arrange2.js
@@ -2,22 +2,8 @@ import * as cg from "../render/core/cg.js";
 import { G2 } from "../util/g2.js";
 import { ControllerBeam } from "../render/core/controllerInput.js";
 
-/*
-How the multiplayer communication works in this scene:
-
-   Each client's input events (press/drag/release/move)
-   only compute the hit point and the current up/down
-   state of that hand or controller. That info, attached
-   to the unique id of this hand of this client, is sent
-   to the master client. The master client then makes all
-   actual scene changes, and broadcasts the new state to
-   all clients.
-*/
-
-window.ss = {
-   S: [],       // Current state of the scene
-   cursors: {}  // Current state of all cursors of all clients
-}
+window.sS = [];
+window.sC = [];
 
 export const init = async model => {
 
@@ -25,6 +11,9 @@ export const init = async model => {
 
    const colors = '#ff0000,#ff4000,#ffff00,#20d020,#0080ff,#6000ff,#e800a0,#ffffff'.split(',');
    const X = .75, move = [0,1.5,0], scale = .3;
+
+   let isFlipped = true;
+   let s = p => clientID == clients[0] ? p : [-p[0],p[1]];
 
    // CREATE THE CONTROLLER BEAMS
 
@@ -95,29 +84,29 @@ export const init = async model => {
          if (window.handtracking) {
             let p = inputEvents.pos(hand);
             if (p)
-               return [ (p[0] - move[0]) / scale,
-                        (p[1] - move[1]) / scale ]; // IF USING HANDTRACKING
+               return s([ (p[0] - move[0]) / scale,
+                          (p[1] - move[1]) / scale ]); // IF USING HANDTRACKING
          }
 
          let h = beams[hand].hitRect(board.getGlobalMatrix());
          if (h)
-            return [h[0],h[1]];                     // IF USING CONTROLLER BEAMS
+            return s([h[0],h[1]]);                     // IF USING CONTROLLER BEAMS
       }
    }
 
    let findPiece = p => {                   // FIND THE FRONT-MOST PIECE AT THE CURSOR
-      for (let n = ss.S.length-1 ; n >= 0 ; n--)
-         if (Math.abs(ss.S[n].p[0] - p[0]) < .1 && Math.abs(ss.S[n].p[1] - p[1]) < .1)
+      for (let n = sS.length-1 ; n >= 0 ; n--)
+         if (Math.abs(sS[n].p[0] - p[0]) < .1 && Math.abs(sS[n].p[1] - p[1]) < .1)
             return n;
    }
 
    let setCursor = (hand, isPressed) => {
-      let id = hand + clientID;
-      if (! ss.cursors[id])
-         ss.cursors[id] = {};
-      ss.cursors[id].isPressed = isPressed;
-      ss.cursors[id].p = hit(hand);
-      server.broadcastGlobal('ss');
+      let id = 2 * clientID + (hand=='left' ? 0 : 1);
+      if (! sC[id])
+         sC[id] = {};
+      sC[id].isPressed = isPressed;
+      sC[id].p = hit(hand);
+      server.broadcastGlobalSlice('sC', id, id+1);
    }
 
    inputEvents.onPress   = hand => setCursor(hand, true);
@@ -125,15 +114,21 @@ export const init = async model => {
    inputEvents.onRelease = hand => setCursor(hand, false);
    inputEvents.onMove    = hand => setCursor(hand, false);
 
-   let states = {};                         // CURSOR STATE THE MASTER CLIENT MAINTAINS
+   let states = [];                         // CURSOR STATE THE MASTER CLIENT MAINTAINS
+
+   let infoText = '';
 
    model.animate(() => {
 
-      ss = server.synchronize('ss');          // GET CURRENT STATE FROM MASTER CLIENT
+      sC = server.synchronize('sC');
 
       if (clientID == clients[0]) {           // IF I AM MASTER CLIENT, MODIFY SCENE STATE
-         for (let id in ss.cursors) {         // BY RESPONDING TO THE CURRENT STATE OF
-	    let cursor = ss.cursors[id];      // ALL CLIENT CURSORS
+
+         for (let id in sC) {         // BY RESPONDING TO THE CURRENT STATE OF CLIENT CURSORS
+
+	    let isMaster = (id >> 1) == clients[0];
+
+	    let cursor = sC[id];
 	    let p = cursor.p;
 
 	    if (! states[id])
@@ -144,15 +139,17 @@ export const init = async model => {
 
 	    if (! state.isPressed && cursor.isPressed) {
                if (p && p[0] < -X) {          // PRESS ON A SHAPE ICON TO CREATE A NEW PIECE
-                  state.n = ss.S.length;
-                  ss.S.push({ type: y2n(p[1]), p: p, c: 7 });
+                  state.n = sS.length;
+                  sS.push({ type: y2n(p[1]), p: p, c: 7 });
                }
                if (p && p[0] > X) {           // PRESS ON A COLOR ICON TO DRAG THAT COLOR
-                  state.c = y2n(p[1]);
-                  state.cp = p;
+                  cursor.c = y2n(p[1]);
+                  cursor.cp = p;
+                  server.broadcastGlobal('sC');
                }
-               if (p)                         // PRESS ON A PIECE TO DRAG IT
+               if (p) {                       // PRESS ON A PIECE TO DRAG IT
                   state.n = findPiece(p);
+               }
             }
 
 	    // ON DRAG
@@ -160,11 +157,15 @@ export const init = async model => {
 	    if (state.isPressed && cursor.isPressed) {
                if (state.n !== undefined) {  // DRAGGING A PIECE
                   if (p)
-                     ss.S[state.n].p = p;
+                     sS[state.n].p = p;
                }
-               if (state.c !== undefined) {  // DRAGGING A COLOR
-                  if (p)
-                     state.cp = p;
+               if (cursor.c !== undefined) {  // DRAGGING A COLOR
+		  if (! isMaster)
+		     frameColor = '#ff0000';
+                  if (p) {
+                     cursor.cp = p;
+                     server.broadcastGlobal('sC');
+                  }
                }
             }
 
@@ -172,23 +173,24 @@ export const init = async model => {
 
 	    if (state.isPressed && ! cursor.isPressed) {
                let n = state.n; 
-               if (n !== undefined && Math.abs(ss.S[n].p[0]) > X)
-                  ss.S.splice(n, 1);
+               if (n !== undefined && Math.abs(sS[n].p[0]) > X)
+                  sS.splice(n, 1);
                delete state.n;               // DRAG A PIECE OFF THE BOARD TO DELETE IT
 
-               if (state.c !== undefined) {
+               if (cursor.c !== undefined) {
                   if (p) {                    // DROP A COLOR ON A PIECE TO CHANGE PIECE COLOR
                      let n = findPiece(p);
                      if (n !== undefined)
-                        ss.S[n].c = state.c;
+                        sS[n].c = cursor.c;
                   }
                }
-               delete state.c;
+               delete cursor.c;
+               server.broadcastGlobal('sC');
             }
 
 	    state.isPressed = cursor.isPressed;
          }
-         server.broadcastGlobal('ss');       // MASTER CLIENT SENDS THE NEW STATE TO ALL CLIENTS
+         server.broadcastGlobal('sS');       // MASTER CLIENT SENDS THE NEW STATE TO ALL CLIENTS
       }
 
       for (let hand in beams)                             // UPDATE THE CONTROLLER BEAMS
@@ -199,30 +201,33 @@ export const init = async model => {
       g2.setColor('#ffffff');
       g2.drawRect(-X,-X,2*X,2*X);                         // DRAW A FRAME AROUND THE BOARD
 
+      g2.text(infoText, 0, .8);
+
       for (let type = 0 ; type < 7 ; type++)              // DRAW THE SHAPE ICONS
-         drawShape(type, [-X*1.12, n2y(type)], colors[7] + 'd0');
+         drawShape(type, s([-X*1.12, n2y(type)]), colors[7] + 'd0');
 
       for (let n = 0 ; n < 7 ; n++) {                     // DRAW THE COLOR ICONS
          g2.setColor(colors[n] + 'd0');
-         g2.fillCurve(32, t => superquadric(t, [X*1.12,n2y(n)], .058));
+         g2.fillCurve(32, t => superquadric(t, s([X*1.12,n2y(n)]), .058));
       }
 
-      for (let n = 0 ; n < ss.S.length ; n++)             // DRAW ALL THE PIECES ON THE BOARD
-         drawShape(ss.S[n].type, ss.S[n].p, colors[ss.S[n].c]);
+      for (let n = 0 ; n < sS.length ; n++)             // DRAW ALL THE PIECES ON THE BOARD
+         drawShape(sS[n].type, s(sS[n].p), colors[sS[n].c]);
 
-      for (let id in ss.cursors) { 
-         let state = states[id];
-         if (state.c !== undefined) {                     // IF COLOR DRAGGING, DRAW COLOR ICON
-            g2.setColor(colors[state.c] + 'd0');
-            g2.fillCurve(32, t => superquadric(t, state.cp, .058));
+      for (let id in sC) { 
+         let cursor = sC[id];
+
+         if (cursor.c !== undefined) {                     // IF COLOR DRAGGING, DRAW COLOR ICON
+            g2.setColor(colors[cursor.c] + 'd0');
+            g2.fillCurve(32, t => superquadric(t, s(cursor.cp), .058));
          }
 
-         let cursor = ss.cursors[id];
          let p = cursor.p;                                // SHOW THE CURSOR ON THE BOARD
          if (p) {
             let x = p[0], y = p[1], r = .014;
             g2.lineWidth(cursor.isPressed ? .01 : .005);
-            g2.setColor('#000000').line([x-r,y-r],[x+r,y+r]).line([x-r,y+r],[x+r,y-r]);
+            g2.setColor('#000000').line(s([x-r,y-r]),s([x+r,y+r]))
+	                          .line(s([x-r,y+r]),s([x+r,y-r]));
          }
       }
    });
